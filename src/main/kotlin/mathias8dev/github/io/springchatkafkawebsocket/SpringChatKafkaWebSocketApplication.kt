1e632f8bae23f5c471bf68a4befc2276cb263797
mathias8dev <kalipemathias.pro@gmail.com>
2024-08-05 14:46:53 +0200

    init: Initialization of the project

---
+++ src/main/kotlin/mathias8dev/github/io/springchatkafkawebsocket/SpringChatKafkaWebSocketApplication.kt
@@ -0,0 +1,11 @@
+package mathias8dev.github.io.springchatkafkawebsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringChatKafkaWebSocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringChatKafkaWebSocketApplication>(*args)
+}
---
+++ src/test/kotlin/mathias8dev/github/io/springchatkafkawebsocket/SpringChatKafkaWebSocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package mathias8dev.github.io.springchatkafkawebsocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringChatKafkaWebSocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
